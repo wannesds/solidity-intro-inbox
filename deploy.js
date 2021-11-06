2927b6819a21246ddc0eee3dc2b4a3ba35948d42
Wannes Dieltiens <50752071+wannesds@users.noreply.github.com>
2021-11-06 19:36:42 +0100

    remade project for new module changes

--- deploy.js
+++ deploy.js
@@ -1,25 +1,27 @@
-const HDWalletProvider = require('truffle-hdwallet-provider')
-const Web3 = require('web3')
-const { interface, bytecode } = require('./compile')
-
-//Whoever is reading this, there's no point in stealing this Eth account
-//It will ONLY be used for development purposes so you'll only find fake Eth
-
-const provider = new HDWalletProvider(
-    'term meadow curious ability combine prize minimum come core dolphin timber cute',
-    'https://rinkeby.infura.io/v3/de5933d8420446749237aba293c34e5e'
-)
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+require("dotenv").config();
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
+  process.env.MNEMONIC,
+  process.env.INFURA_API
+);
+ 
 const web3 = new Web3(provider);
-
+ 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts()
-
-    console.log('Attempting to deploy from account', accounts[0])
-
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] })
-        .send({ gas: '1000000', from: accounts[0] })
-
-    console.log('Contract deployed to', result.options.address)
-}
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+ 
 deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,36 +1,35 @@
-const assert = require('assert') //part of node lib, to make sure that 1 value = 1 other value
-const ganache = require('ganache-cli')
-const Web3 = require('web3') //capital W because constructor func
-const web3 = new Web3(ganache.provider()) //small w bceause instance
-const { interface, bytecode } = require('../compile')
-
-const INITIAL_STRING = 'Hi there!'
-
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+ 
+const { abi, evm } = require('../compile');
+ 
+let accounts;
+let inbox;
+ 
 beforeEach(async () => {
-    //get a list of all accounts
-    accounts = await web3.eth.getAccounts()
-
-    //use one of those accounts to deploy the contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
-        .send({ from: accounts[0], gas: '1000000' })
-})
-
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: ['Hi there!'],
+    })
+    .send({ from: accounts[0], gas: '1000000' });
+});
+ 
 describe('Inbox', () => {
-    it('deploys a contract', () => {
-        assert.ok(inbox.options.address)
-    })
-
-    it('has a default message', async () => {
-        const message = await inbox.methods.message().call()
-        assert.strictEqual(message, INITIAL_STRING);
-
-        
-    })
-    
-    it('chan change the message', async () => {
-        await inbox.methods.setMessage('bye!').send({ from: accounts[0] })
-        const message = await inbox.methods.message().call()
-        assert.strictEqual(message, 'bye!')
-    })
-})
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, 'bye');
+  });
+});
